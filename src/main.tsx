f848b08845a1f2449d8ca74d9fd89db32ebe8bce
sion <noismik97@gmail.com>
2024-09-08 18:37:48 +0900

    feat: 라우터 세팅 및 locale 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ConfigProvider } from 'antd';
+import App from './App.tsx';
+import './index.css';
+
+import koKR from 'antd/locale/ko_KR';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+import { BrowserRouter } from 'react-router-dom';
+
+dayjs.locale('ko');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ConfigProvider locale={koKR}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ConfigProvider>
+  </StrictMode>
+);
