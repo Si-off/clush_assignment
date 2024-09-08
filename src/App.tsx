ad7f711ceb639f6e9a1170f78c2bfa9cc22828b9
sion <noismik97@gmail.com>
2024-09-08 18:40:17 +0900

    feat: 메인페이지 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Layout } from 'antd';
+import styled from 'styled-components';
+import { Content } from 'antd/es/layout/layout';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { SideNav, Calendar } from './components';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Layout style={{ minHeight: '100vh' }}>
+      <Routes>
+        <Route path='/' element={<Navigate to='/calendar' />} />
+        <Route element={<SideNav />}>
+          {/** 달력 */}
+          <Route
+            path='/calendar'
+            element={
+              <Layout>
+                <S.Content>
+                  <Calendar />
+                </S.Content>
+              </Layout>
+            }
+          />
+
+          {/** 할일 */}
+          <Route
+            path='/todo'
+            element={
+              <Layout>
+                <S.Content>
+                  <Calendar />
+                </S.Content>
+              </Layout>
+            }
+          />
+        </Route>
+      </Routes>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
+
+const S = {
+  Content: styled(Content)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 16px;
+    background-color: white;
+    padding: 0px 24px;
+  `,
+};
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import Calendar from './Calendar';
+import DateSelect from './DateSelect';
+import SideNav from './SideNav';
+
+export { Calendar, DateSelect, SideNav };
